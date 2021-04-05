42d61b9439afec68ae210ed427c8df99acfc277e
Caio Henrique <caionwebdev@gmail.com>
2021-04-04 21:20:46 -0300

    some backend changes with .ENV and fetching data

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
-const express = require("express");
-const products = require("./data/products");
+import express from "express";
+import dotenv from "dotenv";
+import products from "./data/products.js";
+
+dotenv.config();
 
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("API is Running");
+  res.send("API is Runningaaasda");
 });
 
 app.get("/api/products", (req, res) => {
@@ -16,4 +19,11 @@ app.get("/api/products/:id", (req, res) => {
   res.json(product);
 });
 
-app.listen(5000, console.log("Server is running on port 5000"));
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+  PORT,
+  console.log(
+    `Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`
+  )
+);
